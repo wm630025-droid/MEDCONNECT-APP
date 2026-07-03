5899195ecf16f2d761413cc7362be98cb554fecd
wafaa <wm630025@gmail.com>
2026-07-03 16:01:19 +0300

    lastinggg19

--- lib/chatScreen.dart
+++ lib/chatScreen.dart
@@ -242,6 +242,7 @@ class _ChatScreenState extends State<ChatScreen> {
           isMe: true,
         ),
       );
+      
     });
     WidgetsBinding.instance.addPostFrameCallback((_) {
       if (_scrollController.hasClients) {
